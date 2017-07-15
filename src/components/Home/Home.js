0e008f3c3056c3d0ed16af8824f1bd6a9bd29f0a
loren pabst <lorenpabst@gmail.com>
2017-07-15 17:02:50 -0600

    built skeleton for home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './Home.css';
+
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="home">
+
+          { router }
+
+      </div>
+    );
+  }
+}
+
+
+export default Home;
