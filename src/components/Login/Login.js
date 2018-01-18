2dd7d501cf6fe7550b0e920d8734fa3be0b06f84
Clayton Pabst <claytonpabst@gmail.com>
2018-01-18 01:43:27 -0700

    build header out for the log in page.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 
+import './Login.css';
+
 
 class Login extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-
+      login:{
+        email:'',
+        password:''
+      },
+      signup:{
+        email:'',
+        emailConfirmation:'',
+        password:'',
+        passwordConfirmation:''
+      }
     }
-  }
-
-  componentDidMount(){
-    
   }
 
   render() {
     return (
       <section className='login_page'>
-        login page
+        <header className='login_header'>
+          <div>
+            <h1>facebook</h1>
+          </div>
+          <div style={{"paddingLeft":"100px"}}>
+            <span>
+              <h6>Email</h6>
+              <input type="text"/>  
+            </span>
+            <span>
+              <h6>Password</h6>
+              <input type="text"/>  
+            </span>
+            <button>Log In</button>
+          </div>
+        </header>
+        <div className="login_create_account_wrapper">
+          <h1>Create a New Account</h1>
+          <h1>It's free and always will be.</h1>
+          <input type="text"/>
+          <input type="text"/>
+          <input type="text"/>
+          <input type="text"/>
+        </div>
       </section>
     );
   }
--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home.js';
+import Login from './components/Login/Login.js';
 
 
 export default (
     <Switch>
         
         <Route component={ Home } path='/' exact />
+        <Route component={ Login } path='/login' exact />
 
     </Switch>
 )
