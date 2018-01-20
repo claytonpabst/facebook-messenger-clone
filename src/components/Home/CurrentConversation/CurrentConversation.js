dfa9ae620987cb01843e4e758c22a9e5af58aae2
loren pabst <lorenpabst@gmail.com>
2018-01-20 11:27:11 -0700

    moved current correspondent info to state

--- src/components/Home/CurrentConversation/CurrentConversation.js
+++ src/components/Home/CurrentConversation/CurrentConversation.js
@@ -8,6 +8,13 @@ class CurrentConversation extends Component {
 
     this.state = {
       userInput: '',
+      currentCorrespondent: {
+        name: 'Clayton Pabst',
+        thumbnail: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/p50x50/20106655_771138229724477_2770561463904245587_n.jpg?oh=113b7dd649105f48177516849be07701&oe=5AF5D52F',
+        friendStatus: "You're friends on Facebook",
+        occupation: 'New Look Pro Studios',
+        location: 'Syracuse, Utah',
+      },
       messages: [
         {
           correspondent: 'Clayton Pabst', 
@@ -36,7 +43,11 @@ class CurrentConversation extends Component {
   }
 
   componentDidMount(){
-    //axios to get current conversation from DB and save it to state
+    /*
+      axios to get the following and save them to state:
+        - current correspondent's info
+        - message history with current correspondent
+    */
   }
 
   render() {
@@ -45,12 +56,12 @@ class CurrentConversation extends Component {
 
         <div className='conversation_wrapper'>
           <div className='correspondent_info_wrapper'>
-            <img src='https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/p50x50/20106655_771138229724477_2770561463904245587_n.jpg?oh=113b7dd649105f48177516849be07701&oe=5AF5D52F' alt='correspondent thumbnail' className='correspondent_thumbnail' />
+            <img src={this.state.currentCorrespondent.thumbnail} alt='correspondent thumbnail' className='correspondent_thumbnail' />
             <div className='correspondent_info'>
-              <h2>Clayton Pabst</h2>
-              <h4>You're friends on Facebook</h4>
-              <p>New Look Pro Studios</p>
-              <p>Lives in Syracuse, Utah</p>
+              <h2>{this.state.currentCorrespondent.name}</h2>
+              <h4>{this.state.currentCorrespondent.friendStatus}</h4>
+              <p>{this.state.currentCorrespondent.occupation}</p>
+              <p>Lives in {this.state.currentCorrespondent.location}</p>
             </div>
           </div>
 
